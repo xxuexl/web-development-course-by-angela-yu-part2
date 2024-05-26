cc49f24f63f82aaa1e325ded5188a08fad1e2088
Xiao Xue <anxxue@gmail.com>
2024-05-26 15:36:12 +0200

    Variable 1 exercise done

--- 1. JS Variables/exercises.js
+++ 1. JS Variables/exercises.js
@@ -1,10 +1,12 @@
 /*Given the existing code below, can you write some code so that their values are switched around?*/
-var a = "3";
-var b = "8";
+let a = "3";
+let b = "8";
 
 /***********Do not change the code above 👆******
 You are NOT allowed to type any numbers nor redeclare the variables a and b. */
-
+let c = a;
+a = b;
+b = c;
 /***********Do not change the code below 👇*******/
 
 console.log("a is " + a);
