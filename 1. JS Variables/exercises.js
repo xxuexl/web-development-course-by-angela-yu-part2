075eb9978b3090ef1f1346e7b41ad6415685672d
Xiao Xue <anxxue@gmail.com>
2024-05-26 15:19:13 +0200

    JS Variables exercise 1

---
+++ 1. JS Variables/exercises.js
@@ -0,0 +1,11 @@
+/*Given the existing code below, can you write some code so that their values are switched around?*/
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆******
+You are NOT allowed to type any numbers nor redeclare the variables a and b. */
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
