3b5a4be8da5e1fb0a82de876e3d2e9a898998f7f
Xiao Xue <anxxue@gmail.com>
2024-06-04 14:03:40 +0200

    Exercise life in weeks

--- 1. JS Variables/exercises2.js
+++ 1. JS Variables/exercises2.js
@@ -1,4 +1,4 @@
-/*1.7 Parameters and arguments*/
+/*1.1 Parameters and arguments*/
 function getMilk(money) {
   console.log("leaveHouse");
   console.log("moveRight");
@@ -10,3 +10,21 @@ function getMilk(money) {
 }
 
 getMilk(5);
+
+/*1.2 Life in weeks: Create a function that tells us how many days, weeks and months we have left if we live until 90 years old.*/
+function lifeInWeeks(age) {
+  let yearsRemaining = 90 - age;
+  let days = yearsRemaining * 365;
+  let weeks = yearsRemaining * 52;
+  let months = yearsRemaining * 12;
+  console.log(
+    "You have " +
+      days +
+      " days, " +
+      weeks +
+      " weeks, and " +
+      months +
+      " months left."
+  );
+}
+lifeInWeeks(30);
