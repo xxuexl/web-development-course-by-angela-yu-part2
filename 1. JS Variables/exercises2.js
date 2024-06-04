0ce6750daf96e0384600e48e41834736a699e999
Xiao Xue <anxxue@gmail.com>
2024-06-04 13:05:24 +0200

    Parameter exercise

---
+++ 1. JS Variables/exercises2.js
@@ -0,0 +1,12 @@
+/*1.7 Parameters and arguments*/
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+
+  let numberOfBottles = money / 1.5; //1.5 the cost per bottle
+  console.log("buy " + numberOfBottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(5);
