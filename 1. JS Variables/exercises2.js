94d0c374ec999a1105e72fdeb6aa8df3dc9414ba
Xiao Xue <anxxue@gmail.com>
2024-07-28 23:54:18 +0200

    Array exercise random name

--- 1. JS Variables/exercises2.js
+++ 1. JS Variables/exercises2.js
@@ -210,3 +210,16 @@ function fizzBuzz() {
 
   console.log(output);
 }
+
+/*2.13 Arrays: Selecting a random name from an array exercise*/
+let names = ["Angela", "Susana", "Matt", "Carl"];
+
+function whosPaying(names) {
+  let numberOfPeople = names.length;
+  let randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  let randomPerson = names[randomPersonPosition];
+
+  return randomPerson + " is going to buy lunch today!";
+}
+
+console.log(whosPaying(names));
