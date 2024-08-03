d5323c8467355d49f1ee633fef087d0d2d1b83f1
Xiao Xue <anxxue@gmail.com>
2024-08-03 18:01:35 +0200

    fizzbuzz exercise with for loop reverse

--- 1. JS Variables/exercises4.js
+++ 1. JS Variables/exercises4.js
@@ -3,3 +3,22 @@
 for (let i = 1; i < 2; i++) {
   console.log(i);
 }
+
+/* Fizzbuzz with for loops */
+let output = [];
+
+function fizzBuzz() {
+  for (let count = 100; count >= 1; count--) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+  }
+  console.log(output);
+}
+fizzBuzz();
