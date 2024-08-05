364b29d105eafc733ceb5389b7bff5348d4e4bc5
Xiao Xue <anxxue@gmail.com>
2024-08-05 23:12:31 +0200

    getElements

--- 2.DOM/1. Dom start/index.js
+++ 2.DOM/1. Dom start/index.js
@@ -1,1 +1,22 @@
-document.query
+document.query;
+
+/*
+1.    
+document.getElementsByTagName("li"); --> 
+If we run this line of code we get an array that contains all the list items.
+
+document.getElementsByTagName("li")[2].style.color = "purple";  -->
+We have to select the item in the array(n. of index) that we want to change.
+
+document.getElementsByTagName("li").length; -->
+If we want to know how many elements there are that have the tag name "li".
+
+
+2.   
+document.getElementsByClassName("btn"); -->
+Select elements based on the name of the class.
+
+document.getElementsByClassName("btn")[0].style.color = "red"; -->
+Even if we have only one item, it gives us back an array, so we still 
+have to select the first item in the array using [0].
+*/
