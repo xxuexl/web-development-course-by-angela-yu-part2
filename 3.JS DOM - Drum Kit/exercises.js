32ca02bf7f39e8ce5c34d3e8fe1a5721c6492eb4
Xiao Xue <anxxue@gmail.com>
2024-08-11 21:15:49 +0200

    Throw error handling

--- 3.JS DOM - Drum Kit/exercises.js
+++ 3.JS DOM - Drum Kit/exercises.js
@@ -173,3 +173,21 @@ try {
   console.log("This always executes");
 }
 console.log("You have reached the end!");
+
+// THROW
+try {
+  const dividend = Number(window.prompt("Enter a dividend: "));
+  const divisor = Number(window.prompt("Enter a divisor: "));
+  if (divisor == 0) {
+    throw new Error("You can't divide by zero");
+  }
+  if (isNaN(dividend) || isNaN(divisor)) {
+    throw new Error("Values must be a number");
+  }
+  const result = dividend / divisor;
+  console.log(result);
+} catch (error) {
+  console.log(error);
+}
+
+console.log("You have reached the end!");
