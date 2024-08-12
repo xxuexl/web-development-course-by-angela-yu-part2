d6a37c648a1a0fe1f598436ef0086951b317419c
Xiao Xue <anxxue@gmail.com>
2024-08-12 23:41:42 +0200

    Promises

--- 3.JS DOM - Drum Kit/exercises.js
+++ 3.JS DOM - Drum Kit/exercises.js
@@ -297,3 +297,29 @@ try {
 }
 
 console.log("You have reached the end!");
+
+/*  ------- PROMISES ---------- */
+
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You walk the dog");
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You clean the kitchen");
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You take out the trash");
+    }, 500);
+  });
+}
